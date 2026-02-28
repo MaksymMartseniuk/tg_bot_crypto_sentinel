8f11c07b4db404f7f61da03fe893bf78d82456c7
MaksymMartseniuk <marcenukm995@gmail.com>
2026-02-28 16:12:09 +0200

    feat(i18n): enhance language handling by adding I18n parameter to process_language_choice function

--- crypto_sentinel/app/handlers/user.py
+++ crypto_sentinel/app/handlers/user.py
@@ -7,7 +7,7 @@ from app.database.requests import set_user
 from app.keyboards.builders import main_menu
 from app.keyboards.settings_kb import get_settings_kb
 from app.database.requests import set_user_language
-from aiogram.utils.i18n import gettext as _
+from aiogram.utils.i18n import gettext as _, I18n
 
 
 user_router=Router()
@@ -18,17 +18,17 @@ async def cmd_start(message:Message,session: AsyncSession):
     await message.answer("Choose language / Виберіть мову",reply_markup=get_settings_kb())
 
 @user_router.callback_query(F.data.startswith("set_lang_"))
-async def process_language_choice(callback: CallbackQuery, session:AsyncSession):
+async def process_language_choice(callback: CallbackQuery, session:AsyncSession,i18n: I18n):
     lang_code = callback.data.split("_")[-1]
-    
+    i18n.current_locale = lang_code
     await set_user_language(session, callback.from_user.id, lang_code)
     await callback.message.edit_text(
-        _("Language set to Ukrainian! 🇺🇦", locale=lang_code) if lang_code == "uk" 
-        else _("Language set to English! 🇺🇸", locale=lang_code)
+        _("Language set to Ukrainian! 🇺🇦") if lang_code == "uk" 
+        else _("Language set to English! 🇺🇸")
     )
     
     await callback.message.answer(
-        _("Welcome ! How can I help you today?", locale=lang_code),
+        _("Welcome ! How can I help you today?"),
         reply_markup=main_menu()
     )
     
